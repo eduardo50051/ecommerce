b41f97034dfab8a8c90e1ff273f709e4fe9567da
eduardo <eduardogomesk123@live.com>
2024-05-07 10:17:07 -0300

    criando_template_login

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -21,6 +22,15 @@ $page->setTpl("index");
 
 
 });
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+	
+	$page->setTpl("index");
+	
+	
+	});
 
 $app->run();
 
