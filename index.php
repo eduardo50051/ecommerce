712bf624bc4c79c2e9acfb0fc03ec8794637541c
eduardo <eduardogomesk123@live.com>
2024-08-02 14:51:24 -0300

    cateogrias crud pronto' git push origin master

--- index.php
+++ index.php
@@ -224,6 +224,8 @@ $app->get("/admin/forgot/sent", function(){
 
 $app->get("/admin/categories", function() {
 
+	User::verifyLogin();
+
 	$categories = Category::listAll();
 
 	$page = new PageAdmin();
@@ -238,6 +240,8 @@ $app->get("/admin/categories", function() {
 
 $app->get("/admin/categories/create", function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("categories-create");
@@ -246,6 +250,8 @@ $app->get("/admin/categories/create", function() {
 });
 
 $app->post("/admin/categories/create", function() {
+
+	User::verifyLogin();
 
 $category = new Category();
 
@@ -258,6 +264,61 @@ exit;
 
 
 });
+
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /ecommerce/admin/categories');
+	exit;
+
+});
+
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);
+
+
+});
+
+
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /ecommerce/admin/categories');
+	exit;
+
+
+});
+
 
 
 
